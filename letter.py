d6590b8a35d3c39411f238f6b8feed96802ac539
Jessica <jessicadianefletcher@gmail.com>
2023-07-30 15:00:37 -0600

    Add letter.py note to my future self

---
+++ letter.py
@@ -0,0 +1,6 @@
+print('Note to Future Me')
+print('7-30-23')
+print('I am excited to get started with python.')
+print('Hopefully, I will get comfortable with backend coding.')
+print('How do things look in the future? Do I get things figured out with practice?')
+print('💕 Jessica')
