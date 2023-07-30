c87776a324e2cabe7c792f7c773be94b70af6606
Jessica <jessicadianefletcher@gmail.com>
2023-07-30 14:52:20 -0600

    Added initials.py project

---
+++ initials.py
@@ -0,0 +1,8 @@
+#Fun Fact: I make amigurumi
+print('  JJJJJJJJJJJ  FFFFFFFF')
+print('       J       F')
+print('       J       F')
+print('J      J       FFFFF')
+print('J      J       F')
+print(' J    J        F')
+print('  JJJJ         F')
