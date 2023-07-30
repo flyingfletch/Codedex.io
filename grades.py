ee071728c2db275e910b2c81f9978ec0e29a7bf0
Jessica <jessicadianefletcher@gmail.com>
2023-07-30 17:24:20 -0600

    Adding grades.py

---
+++ grades.py
@@ -0,0 +1,6 @@
+#Conditional statement practice by checking if grades are above or below 55
+grade = int(input('What was your grade? '))
+if grade >= 55:
+  print("You passed!🥳")
+else:
+  print("You failed.😭")
