3806f5e2a13858053eb1ddd99eb4e4b9df89a6b9
Jessica <jessicadianefletcher@gmail.com>
2023-07-30 16:46:15 -0600

    Adding the currency.py to repository.

---
+++ currency.py
@@ -0,0 +1,6 @@
+print("Let's exchange our trip money back into dollars!")
+pesos = int(input('What do you have left in pesos? '))
+reais = int(input('What do you have left in reais? '))
+soles= int(input('What do you have left in soles? '))
+dollars= int((pesos*.06)+(reais*.21)+(soles*.28))
+print("You have $"+str(dollars)+" left from your trip!")
