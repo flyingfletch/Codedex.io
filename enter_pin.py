0c2613ef05e5dd353223f1009c2337559fe84ddc
Jessica <jessicadianefletcher@gmail.com>
2023-08-20 19:31:13 -0600

    Added enter_pin.py

---
+++ enter_pin.py
@@ -0,0 +1,10 @@
+#WHILE LOOP that keeps asking for pin until the correct pin is entered
+print('BANK OF CODÉDEX')
+
+pin = int(input('Enter your PIN: '))
+
+while pin != 1234:
+  pin = int(input('Incorrect PIN. Enter your PIN again: '))
+
+if pin == 1234:
+  print('PIN accepted!')
