c1f77e3b534ae258fc4e8e9a7a79ad9bc35c1732
Jessica <jessicadianefletcher@gmail.com>
2023-08-23 18:27:39 -0600

    Improve Guess.py

--- guess.py
+++ guess.py
@@ -1,12 +1,14 @@
 #WHILE LOOP number guessing program
 tries = 0
 guess = 0
-
-while guess != 6 and tries < 5:
+import random
+x=0
+x=random.randint(1,9)
+while guess != x and tries < 5:
     guess = int(input("Guess the number:  "))   
     tries= tries + 1
 
-if guess == 6:
+if guess == x:
     print("You got it!")
 else:
-  print("You lose!")
+  print("You lose! The number was",x,"😭")
