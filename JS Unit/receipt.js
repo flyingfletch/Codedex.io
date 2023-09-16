94911e4815202c0e1559386fb8e3b7f5ad57c094
Jessica <jessicadianefletcher@gmail.com>
2023-09-16 12:38:58 -0600

    Add js receipt practice

---
+++ JS Unit/receipt.js
@@ -0,0 +1,14 @@
+// Create a receipt with console.log
+console.log("==================================");
+console.log("          MACEYS RECEIPT");
+console.log("----------------------------------");
+console.log("         Purchase Details");
+console.log("----------------------------------");
+console.log("Item          Quantity    Price($)");
+console.log("----------------------------------");
+console.log("Taquitos          1           7.89");
+console.log("Hot Dogs          1           5.19");
+console.log("Gala Apples       1           5.57");
+console.log("----------------------------------");
+console.log("Total                        18.65");
+console.log("==================================");
