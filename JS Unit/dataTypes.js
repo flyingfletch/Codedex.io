289759155d8ebcfb12fbb13f6e36995adefcb654
Jessica <jessicadianefletcher@gmail.com>
2023-09-16 21:01:01 -0600

    Added variable and data type js practice

---
+++ JS Unit/dataTypes.js
@@ -0,0 +1,8 @@
+// Exploring different data types
+let favNumber = 666;
+let favSong = "Lost Umbrella";
+const catsRBest = true;
+
+console.log("My favorite number is " + favNumber);
+console.log("My favorite song is " + favSong);
+console.log("Are cats the best animal? " + catsRBest);
