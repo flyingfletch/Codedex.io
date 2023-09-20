291081093dabbcd80e715929581c2584a374812a
Jessica <jessicadianefletcher@gmail.com>
2023-09-19 19:29:57 -0600

    Added and Improved bmi.js

--- JS Unit/bmi.js
+++ JS Unit/bmi.js
@@ -10,3 +10,13 @@ let height = heightMeters;
 let bmi = mass/(height**2);
 
 console.log("Your bmi is " + bmi);
+
+if (bmi <= 18.4) {
+    console.log("BMI says you're Underweight.");
+} else if (bmi >= 18.5 && bmi <= 24.9) {
+    console.log("BMI says you're Normal.");
+} else if (bmi >= 25.0 && bmi <= 39.9) {
+    console.log("BMI says you're Overweight.");
+} else {
+    console.log("BMI says you're Obese.");
+}
