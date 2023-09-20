a68862b5e78952e6f92d305cf8c3a0ebbccaacd0
Jessica <jessicadianefletcher@gmail.com>
2023-09-19 20:53:29 -0600

    Added good-morning.js if else practice

---
+++ JS Unit/good-morning.js
@@ -0,0 +1,12 @@
+// Practicing If Else Conditional Statements
+let hour = 5;
+
+if (hour < 6) {
+    console.log("Hey Night Owl. 🦉");
+} else if (hour >= 6 && hour < 12) {
+    console.log("Good morning. 🌞");
+} else if (hour >= 12 && hour <= 19) {
+    console.log("Good afternoon. 😎");
+} else {
+    console.log("Good evening. 🌛");
+}
